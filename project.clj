745177f3f50f1d1e642e70dd881ed7888d4fa584
Gregg Reynolds <reynolds-gregg@norc.org>
2015-12-10 18:44:32 -0600

    initial commit: polymer 1.2.3 paper elements

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject miraj.polymer/paper "1.2.3-SNAPSHOT"
+  :description "Polymer Paper Elements"
+  :url "https://github.com/mobileink/polymer.paper"
+  :license {:name "BSD License"
+            :url "http://polymer.github.io/LICENSE.txt"})
