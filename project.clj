7c61a202ea0a8263e74643b523e26bab833a2ee3
Gregg Reynolds <reynolds-gregg@norc.org>
2015-12-13 01:14:34 -0600

    migrate elements to polymer.elements.paper, add tag fns

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject polymer/paper "1.2.3-SNAPSHOT"
-  :description "Polymer Paper Elements"
+  :description "Polymer Paper markup functions"
   :url "https://github.com/mobileink/polymer.paper"
   :license {:name "BSD License"
-            :url "http://polymer.github.io/LICENSE.txt"})
+            :url "http://polymer.github.io/LICENSE.txt"}
+  :dependencies [[org.clojure/clojure "1.8.0-RC3"]
+                 [polymer.elements/paper "1.2.3-SNAPSHOT"]])
