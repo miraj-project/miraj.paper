5d936d6aeb22b778f0ca670a500e33e050e3f745
Gregg Reynolds <reynolds-gregg@norc.org>
2015-12-15 08:43:09 -0600

    EPL license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject polymer/paper "1.2.3-SNAPSHOT"
   :description "Polymer Paper markup functions"
   :url "https://github.com/mobileink/polymer.paper"
-  :license {:name "BSD License"
-            :url "http://polymer.github.io/LICENSE.txt"}
+  :license {:name "EPL"
+            :url "http://opensource.org/licenses/eclipse-1.0.php"}
   :dependencies [[org.clojure/clojure "1.8.0-RC3"]
                  [miraj/markup "0.1.0-SNAPSHOT"]
                  [polymer/elements.paper "1.2.3-SNAPSHOT"]])
