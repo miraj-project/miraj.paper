a1939df332f6cb977031a10b9d7df1a103d72391
Gregg Reynolds <reynolds-gregg@norc.org>
2017-01-04 12:36:15 -0600

    checkpoint
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject polymer/paper "1.2.3-SNAPSHOT"
-  :description "Polymer Paper markup functions"
+(defproject miraj.polymer/paper "1.2.3-SNAPSHOT"
+  :description "Miraj Polymer Paper markup functions"
   :url "https://github.com/mobileink/polymer.paper"
   :license {:name "EPL"
             :url "http://opensource.org/licenses/eclipse-1.0.php"}
-  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [miraj/markup "0.1.0-SNAPSHOT"]])
--- src/miraj/paper.clj
+++ src/miraj/paper.clj
@@ -1,6 +1,4 @@
-(ns polymer.paper
-;;  (:refer-clojure :exclude [map meta time])
-  (:require [miraj.markup :refer [make-resource-fns]]))
+(ns miraj.paper)
 
 (alter-meta! *ns*
              (fn [m] (assoc m
@@ -8,28 +6,48 @@
                             :resource-type :polymer
                             :resource-pfx "bower_components")))
 
-;;             (fn [m] (assoc m :co-ns true)))
+(def pfx "bower_components")
 
-   ;; 'behaviors		:behaviors 	"paper-behaviors/paper-behaviors"
-   ;; [polymer.paper.behaviors :refer :all]
-   ;; [polymer.paper.behavior :refer [button checked-element inky-focus ripple]]
+(def components
+  ;; entries:  [:fn-tag  [elt-kw elt-uri & docstring]]
+  {:badge [:paper-badge "paper-badge/paper-badge.html"]
+   :button [:paper-button "paper-button/paper-button.html"]
+   :card [:paper-card "paper-card/paper-card.html"]
+   :checkbox [:paper-checkbox "paper-checkbox/paper-checkbox.html"]
+   :dialog [:paper-dialog "paper-dialog/paper-dialog.html"]
+   :dialog-behavior [:paper-dialog-behavior "paper-dialog-behavior/paper-dialog-behavior.html"]
+   :dialog-scrollable [:paper-dialog-scrollable "paper-dialog-scrollable/paper-dialog-scrollable.html"]
+   :drawer-panel [:paper-drawer-panel "paper-drawer-panel/paper-drawer-panel.html"]
+   :dropdown-menu [:paper-dropdown-menu "paper-dropdown-menu/paper-dropdown-menu.html"]
+   :fab [:paper-fab "paper-fab/paper-fab.html"]
+   :header-panel [:paper-header-panel "paper-header-panel/paper-header-panel.html"]
+   :icon-button [:paper-icon-button "paper-icon-button/paper-icon-button.html"]
+   :input [:paper-input "paper-input/paper-input.html"]
+   :textarea [:paper-textarea "paper-input/paper-textarea.html"]
+   :item [:paper-item "paper-item/paper-item.html"]
+   :listbox [:paper-listbox "paper-listbox/paper-listbox.html"]
+   :material [:paper-material "paper-material/paper-material.html"]
+   :menu [:paper-menu "paper-menu/paper-menu.html"]
+   :menu-shared-styles [:paper-menu-shared-styles "paper-menu/paper-menu-shared-styles.html.html"]
+   :submenu [:paper-submenu "paper-menu/paper-submenu.html"]
+   :menu-button [:paper-menu-button "paper-menu-button/paper-menu-button.html"]
+   :menu-button-animations [:paper-menu-button-animations "paper-menu-button/paper-menu-button-animations.html"]
+   :progress [:paper-progress "paper-progress/paper-progress.html"]
+   :radio-button [:paper-radio-button "paper-radio-button/paper-radio-button.html"]
+   :radio-group [:paper-radio-group "paper-radio-group/paper-radio-group.html"]
+   :ripple [:paper-ripple "paper-ripple/paper-ripple.html"]
+   :scroll-header-panel [:paper-scroll-header-panel "paper-scroll-header-panel/paper-scroll-header-panel.html"]
+   :slider [:paper-slider "paper-slider/paper-slider.html"]
+   :spinner [:paper-spinner "paper-spinner/paper-spinner.html"]
+   :styles [:paper-styles "paper-styles/paper-styles.html"]
+   :tab [:paper-tab "paper-tabs/paper-tab.html"]
+   :tabs [:paper-tabs "paper-tabs/paper-tabs.html"]
+   :toast [:paper-toast "paper-toast/paper-toast.html"]
+   :toggle-button [:paper-toggle-button	"paper-toggle-button/paper-toggle-button.html"]
+   :toolbar [:paper-toolbar "paper-toolbar/paper-toolbar.html"]
+   :tooltip [:paper-tooltip "paper-tooltip/paper-tooltip"]})
 
-;; [polymer.paper :refer [input textarea]]
-
-;; paper-dialog-common.css
-;; paper-dialog-shared-styles.html
-
-;; [polymer.paper :as paper :refer :all] => 'elements :elements "paper-elements/paper-elements"
-
-;; [polymer.paper.item    :as item    :refer [behavior body shared-styles icon]]
-;; [polymer.paper.menu    :as menu    :refer [button button-animations shared-styles submenu]]
-;; [polymer.paper.spinner :as spinner :refer [behavior lite styles]]
-;; [polymer.paper.styles  :as styles  :refer :all]
-;; [polymer.paper.styles  :as styles  :refer [color default-theme demo-pages shadow typography]]
-
-;; paper-tabs-icons.html - iron-iconset-svg
-
-(def polymer-paper-tags
+#_(def polymer-paper-tags
   ; fn-tag,  elt-kw,  elt-uri,  docstring
   [['badge		:paper-badge	 	"paper-badge/paper-badge.html"
     "tag: <paper-badge>
@@ -92,7 +110,8 @@ uri: paper-card/paper-card.html docstring"]
 	"paper-slider/paper-slider.html docstring"]
    ['spinner		:paper-spinner		"paper-spinner/paper-spinner.html"
 	"paper-spinner/paper-spinner.html docstring"]
-
+   ['styles		:paper-styles		"paper-styles/paper-styles.html"
+	"paper-styles/paper-styles.html docstring"]
    ['tab		:paper-tab		"paper-tabs/paper-tab.html"
 	"paper-tabs/paper-tab.html docstring"]
    ['tabs		:paper-tabs		"paper-tabs/paper-tabs.html"
@@ -106,4 +125,5 @@ uri: paper-card/paper-card.html docstring"]
    ['tooltip		:paper-tooltip		"paper-tooltip/paper-tooltip"
     "paper-tooltip/paper-tooltip"]])
 
-(make-resource-fns :html polymer-paper-tags)
+#_(make-resource-fns :html polymer-paper-tags)
+
