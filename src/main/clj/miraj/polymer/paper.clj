83822c9d1762438cacd166ce53bfe876c8f80c01
Gregg Reynolds <reynolds-gregg@norc.org>
2017-03-24 10:38:48 -0500

    switch to new gen method using load "foo_impl"
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

--- src/main/clj/miraj/polymer/paper.clj
+++ src/main/clj/miraj/polymer/paper.clj
@@ -1,4 +1,15 @@
 (ns miraj.polymer.paper
+  ;; (:refer-clojure :exclude [list meta])
   (:require [miraj.co-dom :as codom]))
+
+(alter-meta! *ns* (fn [m] (assoc m :miraj/miraj {:miraj/elements true
+                                                 :miraj/nss '[]
+                                                 :miraj/codom ""
+                                                 :miraj/assets
+                                                 {:miraj/bower
+                                                  [
+
+                                                   ]
+                                                   :miraj/base ""}})))
 
 (load "paper_impl")
--- src/main/clj/miraj/polymer/paper/input.clj
+++ src/main/clj/miraj/polymer/paper/input.clj
@@ -1,4 +1,14 @@
 (ns miraj.polymer.paper.input
   (:require [miraj.co-dom :as codom]))
 
+(alter-meta! *ns* (fn [m] (assoc m :miraj/miraj {:miraj/elements true
+                                                 :miraj/nss '[]
+                                                 :miraj/codom ""
+                                                 :miraj/assets
+                                                 {:miraj/bower
+                                                  [
+
+                                                   ]
+                                                   :miraj/base ""}})))
+
 (load "input_impl")
